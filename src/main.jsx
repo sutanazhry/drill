747fccabb60ec25c9fa2c80406bfef32efd92b7c
Sutan Azhary <sutanazhry@gmail.com>
2025-05-15 10:37:01 +0700

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QuizApp from "./QuizApp";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QuizApp />
+  </React.StrictMode>
+);
